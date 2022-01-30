ab332aacdbe26f78918cc034b8ae01355360ef84
AlexandroDRK <alexsandro060302@gmail.com>
2022-01-30 14:50:35 -0300

    Gem's update and minor changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  # Defines the root path route ("/")  # root "articles#index"
+
+end
+
+VotaPrato::Application.routes.draw do
+match 'restaurantes', controller: 'restaurantes', action: 'index', via: 'get'
+end
+
+VotaPrato::Application.routes.draw do
+match 'ola' => 'ola_mundo#index', via: 'get'
 end
