f5c14ba2eec409c70ac78087af342ceabe10d937
AlexandroDRK <alexsandro060302@gmail.com>
2022-02-13 11:09:49 -0300

    Adding CSV exporting Controller_method on action index and helper_link_to for exports data values

--- app/controllers/restaurantes_controller.rb
+++ app/controllers/restaurantes_controller.rb
@@ -1,5 +1,11 @@
 class RestaurantesController < ApplicationController
+  require 'csv'
   def index
     @restaurantes = Restaurante.order :nome
+
+    respond_to do |format|
+      format.html
+      format.csv { send_data @restaurantes.to_csv, filename: "restaurantes-#{Date.today}.csv" }
+    end
   end
 end
--- app/models/restaurante.rb
+++ app/models/restaurante.rb
@@ -1,4 +1,16 @@
 class Restaurante < ApplicationRecord
     validates :nome , :endereco , :especialidade, presence: { message: "Preencher todos os campos"}
     validates :nome , uniqueness: {message: "O nome #{:nome} jâ foi cadastrado"}
+    def self.to_csv
+      attributes = %w{nome endereco especialidade}
+
+    CSV.generate(headers: true) do |csv|
+      csv << attributes
+
+      all.each do |user|
+        csv << attributes.map{ |attr| user.send(attr) }
+      end
+    end
+  end
 end
+
